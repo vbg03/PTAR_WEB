6563d67042d91a5dcfb3ef33d55b68f5692ccbaf
vbg03 <valeria.bowers@uao.edu.co>
2026-02-13 14:10:36 -0500

    terminando landing page

--- PTAR_WEB/src/App.jsx
+++ PTAR_WEB/src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
 import Header from './components/Header/header.jsx'
+import Inicio from './components/Inicio/inicio.jsx'
 
 function App() {
   return (
     <div className="ptar-app">
       <Header />
-      <main className="ptar-hero">
-        <div className="ptar-hero__canvas">
-          <h1>EL VIAJE DEL AGUA</h1>
-          <p>
-            Un recorrido por la planta de tratamiento de aguas residuales (PTAR) de la
-            UAO
-          </p>
-          <button type="button">Iniciar el Recorrido</button>
-        </div>
-      </main>
+      <Inicio />
     </div>
   )
 }
---
+++ PTAR_WEB/src/components/Inicio/inicio.jsx
@@ -0,0 +1,18 @@
+import './inicio.css'
+
+function Inicio() {
+  return (
+    <main className="ptar-hero">
+      <div className="ptar-hero__canvas">
+        <h1>EL VIAJE DEL AGUA</h1>
+        <p>
+          Un recorrido por la planta de tratamiento de aguas residuales (PTAR) de la
+          UAO
+        </p>
+        <button type="button">Iniciar el Recorrido</button>
+      </div>
+    </main>
+  )
+}
+
+export default Inicio
